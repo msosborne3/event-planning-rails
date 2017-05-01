968b33ed799988a9a4a4a9e0960fdf1bb41405b3
msosborne3 <msosborne@ymail.com>
2017-05-01 16:45:16 -0500

    Create migrations for event_rsvps and events

---
+++ db/migrate/20170501204314_create_event_rsvps.rb
@@ -0,0 +1,10 @@
+class CreateEventRsvps < ActiveRecord::Migration[5.0]
+  def change
+    create_table :event_rsvps do |t|
+      t.integer :user_id
+      t.integer :event_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430215645) do
+ActiveRecord::Schema.define(version: 20170501204314) do
+
+  create_table "event_rsvps", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.string   "location"
+    t.datetime "time"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
